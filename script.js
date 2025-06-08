b5a3ba8f98e27a8600ccfb448a9db9a248ab3a45
mahesh-desai-10 <mahesh.desai.work@gmail.com>
2025-06-08 11:52:27 +0530

    Complete UI design and finalize CSS styling

---
+++ script.js
@@ -0,0 +1,11 @@
+// let btn = document.querySelector(".search-icon");
+let btn = document.querySelector(".pronunciation");
+// let word = document.querySelector(".search-box");
+let sound = document.querySelector(".word-audio");
+
+// btn.addEventListener("click", () => {
+//   console.log(word.value);
+//   sound.play();
+// });
+
+
